8406b3aaa05890e7f00c23ad676bd41cb7ffd6c6
Adesh <adesh02092000@gmail.com>
2025-11-01 13:55:56 +0530

    Add post link

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,24 @@ import { getQueryClient, trpc } from "@/trpc/server"
 import { ClientGreeting } from "./client-greeting"
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 export default async function Home() {
   const queryClient = getQueryClient()
   void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "world" }))
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ErrorBoundary fallback={<div>Error occurred</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <ClientGreeting />
-        </Suspense>
-      </ErrorBoundary>
-    </HydrationBoundary>
+    <div className="h-screen w-screen flex justify-center items-center flex-col gap-10">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ErrorBoundary fallback={<div>Error occurred</div>}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <ClientGreeting />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrationBoundary>
+      <Link href="/post">
+        <Button className="cursor-pointer">Go to Post Page</Button>
+      </Link>
+    </div>
   )
 }
